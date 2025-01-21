23a666a173ef2aeb530d41c3c05e83f3d0a91828
giovanniAfro <giovanni.cacciacavanna@gmail.com>
2025-01-21 19:56:01 +0100

    another one

---
+++ ft_strcmp.s
@@ -0,0 +1,34 @@
+section .text
+global  ft_strcmp
+
+ft_strcmp:
+    cmp rdi, 0
+    je .error
+    cmp rsi, 0
+    je .error
+
+    xor rax, rax
+
+.loop:
+    mov dl, byte [rdi + rax]
+    mov cl, byte [rsi + rax]
+    cmp dl, cl
+    jne .difference
+    cmp dl, 0
+    je .doneinm 
+    inc rax
+    jmp .loop
+
+.difference:
+    sub dl, cl
+    movsx rax, dl
+    ret
+
+.done:
+    xor rax, rax
+    ret
+
+.error:
+    mov rax, -1
+    ret
+
